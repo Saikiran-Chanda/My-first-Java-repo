e2364b56d54b37bbd0cee8755b20b0b4cdc1b8ce
Saikiran-Chanda <167888508+Saikiran-Chanda@users.noreply.github.com>
2024-12-22 14:29:10 +0530

    Create device.java

---
+++ device.java
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="external.js"></script>
+</head>
+<body>
+    <h1>WELCOME TO JAVASCRIPT</h1>
+    <!-- inline -->
+
+    <!-- <button onclick="window.alert('welcome to java')">CLICK</button> -->
+    <!-- <button onclick="window.confirm('welcome')">SUBMIT</button> -->
+
+<!-- internal -->
+
+   <script>
+    // window.alert("hii.....guys")
+    // window.confirm("hiii....")
+   </script>
+</body>
+</html>
